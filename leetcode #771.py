f2e1e17ddae2800c6cd8f7539283b18ab385db46
seyoung755 <seyoung7555@naver.com>
2020-12-26 21:50:05 +0900

    counter

--- leetcode #771.py
+++ leetcode #771.py
@@ -1,15 +1,10 @@
+import collections
+
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-         
-        result = {}
+        result = collections.Counter(stones)
         count = 0
-        for s in stones:
-            if s not in result:
-                result[s] = 1
-            else:
-                result[s] += 1
 
         for j in jewels:
-            if j in result:
-                count += result[j]
-        return count
+            count += result[j]
+        return count        
